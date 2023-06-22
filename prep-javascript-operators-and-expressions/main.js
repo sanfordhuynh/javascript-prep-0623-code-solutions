ea70a4ebd0e4a681fcf777dc5412912089f1ec6e
Sanford Huynh <huynhsanford@gmail.com>
2023-06-22 04:52:44 +0000

    Completed javascript-prep-operator exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,44 @@
+const number1 = 7;
+const number2 = 4;
+
+const product = number1 * number2;
+
+console.log(product);
+console.log(typeof product);
+
+const charge = 100;
+const payment = 20;
+
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+const tests = 5;
+const assignments = 10;
+const final = 2;
+
+const grade = (tests + assignments + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Sanford';
+const lastName = 'Huynh';
+
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+const ph = 6;
+const isAcidic = ph > 7;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+const numberOfSoldiers = 3000;
+const isSparta = numberOfSoldiers === 300;
+
+console.log(isSparta);
+console.log(typeof isSparta);
+
+const nickname = fullName + ' ' + 'is the GOAT';
+console.log(nickname);
+console.log(typeof nickname);
