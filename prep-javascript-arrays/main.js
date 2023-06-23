e1aaa47946cc4e152eb8f22203f26caa9a2a30c5
Sanford Huynh <huynhsanford@gmail.com>
2023-06-23 04:48:08 +0000

    Completed prep-javascript array exercise

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,16 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+
+console.log(languages);
+
+const firstElement = 'the first element of the array is ' + languages[0];
+console.log(firstElement);
+
+const thirdElement = 'the 3rd element of the array is ' + languages[3];
+console.log(thirdElement);
+
+const length = 'the length of the array is ' + languages.length;
+console.log(length);
+
+const lastElement =
+  'the last element of the array is ' + languages[languages.length - 1];
+console.log(lastElement);
