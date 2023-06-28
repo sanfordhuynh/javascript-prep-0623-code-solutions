cb6bbfe154f7fdc405228ff25048613d4550a241
Sanford Huynh <huynhsanford@gmail.com>
2023-06-28 22:56:22 +0000

    Completed prep-array-challenge

---
+++ prep-array-challenge/src/head.js
@@ -0,0 +1,7 @@
+/* exported head */
+
+function head(array) {
+  const first = array[0];
+  // console.log(first);
+  return first;
+}
---
+++ prep-array-challenge/src/last.js
@@ -0,0 +1,6 @@
+/* exported last */
+function last(array) {
+  const last = array[array.length - 1];
+  console.log(last);
+  return last;
+}
